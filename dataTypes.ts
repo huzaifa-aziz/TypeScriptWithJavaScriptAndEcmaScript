81337232bcd799429ca5f31d93396d52311f94e8
Huzaifa Aziz <106053543+huzaifa-aziz@users.noreply.github.com>
2024-02-23 10:25:03 -0800

    Add files via upload

---
+++ dataTypes.ts
@@ -0,0 +1,41 @@
+//number
+let age : number = 32;
+let height : number = 70;
+
+//string
+
+let name : string = "mySelf";
+let message : string = "hello buddy!"
+
+//boolean
+
+let isAvailable : boolean = false;
+let isNotAvailable : boolean = true;
+
+//array
+
+let number : number[] = [2,3,4,6,7,8];
+let color = ["red","blue","orange"];
+
+//tuple
+
+let person : [string,number]  = ["mySelf",36];
+
+//enum
+
+enum direction {
+    up,
+    down,
+    left,
+    right
+}
+
+//any
+
+let variable : any  = 10;
+variable = "name";
+variable = false
+
+//object
+
+let person : object = { name: "John", age: 30 };
