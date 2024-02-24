3b400368dff6c3b3820f7d9782f01a00d7d6e56e
Huzaifa Aziz <106053543+huzaifa-aziz@users.noreply.github.com>
2024-02-24 10:32:18 -0800

    Add files via upload

---
+++ artirhmaticOperators.ts
@@ -0,0 +1,19 @@
+//Addition
+let num1 = 2;
+let num2 = 4;
+console.log(num1+num2);
+
+//Substraction
+let difference: number = 10 - 5; // difference = 5
+
+let product: number = 6 * 4; // product = 24
+
+let quotient: number = 20 / 5; // quotient = 4
+
+let remainder: number = 10 % 3; // remainder = 1
+
+let x: number = 5;
+x++; // x = 6
+
+let y: number = 8;
+y--; // y = 7
