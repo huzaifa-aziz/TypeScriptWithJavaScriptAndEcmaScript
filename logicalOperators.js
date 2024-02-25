ab88f857fee636922c50cbbc26ebe9fdd9133166
Huzaifa Aziz <106053543+huzaifa-aziz@users.noreply.github.com>
2024-02-25 08:40:12 -0800

    Add files via upload

---
+++ logicalOperators.js
@@ -0,0 +1,15 @@
+//Logical AND
+var a = true;
+var b = false;
+var result1 = a && b; // result1 = false
+var result2 = true && true; // result2 = true
+//Logical OR
+var c = true;
+var d = false;
+var result3 = c || d; // result3 = true
+var result4 = false || false; // result4 = false
+//Logical NOT
+var e = true;
+var f = false;
+var result5 = !e; // result5 = false
+var result6 = !f; // result6 = true
---
+++ logicalOperators.ts
@@ -0,0 +1,19 @@
+//Logical AND
+let a: boolean = true;
+let b: boolean = false;
+let result1: boolean = a && b; // result1 = false
+let result2: boolean = true && true; // result2 = true
+
+
+//Logical OR
+let c: boolean = true;
+let d: boolean = false;
+let result3: boolean = c || d; // result3 = true
+let result4: boolean = false || false; // result4 = false
+
+//Logical NOT
+let e: boolean = true;
+let f: boolean = false;
+let result5: boolean = !e; // result5 = false
+let result6: boolean = !f; // result6 = true
+
