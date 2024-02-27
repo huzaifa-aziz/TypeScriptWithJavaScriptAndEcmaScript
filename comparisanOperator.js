b813f2b94cd5fb68118e296c2b5cafd9fc12d7dd
Huzaifa Aziz <106053543+huzaifa-aziz@users.noreply.github.com>
2024-02-27 10:38:38 -0800

    Add files via upload

---
+++ comparisanOperator.js
@@ -0,0 +1,38 @@
+"use strict";
+// Equal To
+Object.defineProperty(exports, "__esModule", { value: true });
+var num1 = 3;
+var num = 'string';
+console.log(num1 == num2);
+// Strict Equal To
+var name = "huzaifa";
+var name2 = "Aziz";
+console.log(name === name2);
+// Not Equal to
+var num3 = 3;
+var num4 = 5;
+console.log(num1 != num2);
+//Strict Not Equal To
+var num6 = 3;
+var num7 = "5";
+console.log(num1 !== num2);
+//Greater Than
+var num5 = 3;
+var num8 = 5;
+console.log(num1 > num2);
+//Less than
+var num9 = 3;
+var num0 = 5;
+console.log(num1 < num2);
+//Greater Than Or Equal To
+var num11 = 3;
+var num22 = 5;
+console.log(num1 >= num2);
+//Less Than Or Equal To
+var num13 = 3;
+var num14 = 5;
+console.log(num1 <= num2);
+var myName = null;
+var defaultName = 'demon';
+var displayName = myName !== null && myName !== void 0 ? myName : defaultName;
+console.log(displayName);
