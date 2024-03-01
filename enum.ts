89572147d4fb80695e12c8d3367884541ae57e72
Huzaifa Aziz <106053543+huzaifa-aziz@users.noreply.github.com>
2024-03-01 06:54:32 -0800

    Add files via upload

---
+++ enum.ts
@@ -0,0 +1,53 @@
+// Define the Direction enum
+enum Direction {
+    Up, 
+    Down,
+    Left,
+    Right
+  }
+  
+  // Specify the type of the direction variable explicitly
+  let direction: Direction = Direction.Left;
+  
+  // Now the switch statement should work without errors
+  switch (direction) {
+    case Direction.Up:
+      console.log('Going Up');
+      break;
+    case Direction.Down:
+      console.log('Going Down');
+      break;
+    case Direction.Left:
+      console.log('Going Left');
+      break;
+    case Direction.Right:
+      console.log('Going Right');
+      break;
+  }
+// Define the Direction enum
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
+  }
+  
+  // Specify the type of the direction variable explicitly
+  let direction: Direction = Direction.Left;
+  
+  // Now the switch statement should work without errors
+  switch (direction) {
+    case Direction.Up:
+      console.log('Going Up');
+      break;
+    case Direction.Down:
+      console.log('Going Down');
+      break;
+    case Direction.Left:
+      console.log('Going Left');
+      break;
+    case Direction.Right:
+      console.log('Going Right');
+      break;
+  }
+export{}    
