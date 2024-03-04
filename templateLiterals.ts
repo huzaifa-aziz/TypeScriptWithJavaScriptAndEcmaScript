2ffba49c7dbff6472ea28a7c170d36adf9c34e3c
Huzaifa Aziz <106053543+huzaifa-aziz@users.noreply.github.com>
2024-03-04 09:55:31 -0800

    Add files via upload

---
+++ templateLiterals.ts
@@ -0,0 +1,11 @@
+let name = 'jhoolay laal';
+let age = 30;
+let city = 'karachi';
+
+// Using template literals
+let message = `Hello, my name is ${name}, I am ${age} years old, and I live in ${city}.`;
+
+console.log(message); // Output: Hello, my name is John, I am 30 years old, and I live in New York.
+
+
+export{}
