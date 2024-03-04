4a2a1e11fa4a81d31e3b0f7acb0b22f84d465ffa
Huzaifa Aziz <106053543+huzaifa-aziz@users.noreply.github.com>
2024-03-04 09:55:49 -0800

    Add files via upload

---
+++ templateLiterals.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var name = 'John';
+var age = 30;
+var city = 'New York';
+// Using template literals
+var message = "Hello, my name is ".concat(name, ", I am ").concat(age, " years old, and I live in ").concat(city, ".");
+console.log(message); // Output: Hello, my name is John, I am 30 years old, and I live in New York.
